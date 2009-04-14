d8c4ddbc776dc0f14eb2e395e41c3fef43a445d5
Federico Fissore <federico@fissore.org>
2009-04-14 08:41:41 +0000

    code clean up :)

--- jrecordbind/test/it/assist/jrecordbind/test/MyGGEnumRecord.java
+++ jrecordbind/test/it/assist/jrecordbind/test/MyGGEnumRecord.java
@@ -4,10 +4,6 @@ import it.assist_si.schemas.jrb.ggenum.GGEnumRecord;
 
 public class MyGGEnumRecord extends GGEnumRecord {
 
-  public MyGGEnumRecord() {
-    System.out.println("yeppa yeppa");
-  }
-
   @Override
   public MyEnum getMyEnum() {
     return (MyEnum) super.getMyEnum();
