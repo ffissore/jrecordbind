e74125bfe714af79ebb65811aa953259260b402e
Fabio Marinelli <f.marinelli@gmail.com>
2014-08-21 17:48:52 +0200

    Added test case for multi classloader.

---
+++ jrecordbind-pojo/src/main/java/it/assist/jrecordbind/classloader/converter/RecordDateConverter.java
@@ -0,0 +1,53 @@
+/*
+ * JRecordBind, fixed-length file (un)marshaller
+ * Copyright 2009, Assist s.r.l., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package it.assist.jrecordbind.classloader.converter;
+
+import it.assist.jrecordbind.Converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class RecordDateConverter implements Converter {
+
+  private SimpleDateFormat simpleDateFormat;
+
+  public RecordDateConverter() {
+    simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+  }
+
+  public Object convert(String value) {
+    try {
+      Calendar instance = Calendar.getInstance();
+      instance.setTime(simpleDateFormat.parse(value));
+      return instance;
+    } catch (ParseException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public String toString(Object value) {
+    return simpleDateFormat.format(((Calendar) value).getTime());
+  }
+
+}
---
+++ jrecordbind-pojo/src/main/java/it/assist/jrecordbind/classloader/converter/RecordFloatConverter.java
@@ -0,0 +1,39 @@
+/*
+ * JRecordBind, fixed-length file (un)marshaller
+ * Copyright 2009, Assist s.r.l., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package it.assist.jrecordbind.classloader.converter;
+
+import it.assist.jrecordbind.Converter;
+
+public class RecordFloatConverter implements Converter {
+
+  public Object convert(String value) {
+    return Float.valueOf(value.substring(0, 1) + "." + value.substring(1));
+  }
+
+  public String toString(Object value) {
+    String f = value.toString();
+
+    return f.substring(f.indexOf(".") - 1, f.indexOf(".")) + f.substring(f.indexOf(".") + 1, f.indexOf(".") + 3);
+  }
+
+}
